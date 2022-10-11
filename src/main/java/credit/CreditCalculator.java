926d2ef0c36386c98fc343b104a9d4cf945389b8
Averianov Yuriy <averianovj@gmail.com>
2022-10-11 20:34:54 +0300

    first commit refact(change)

--- src/main/java/credit/CreditCalculator.java
+++ src/main/java/credit/CreditCalculator.java
@@ -3,15 +3,16 @@ package credit;
 
 public class CreditCalculator {
 
-    public int monthPaymentCalc(int sum, int percent, int months) {
+
+    public double monthPaymentCalc(int sum, int percent, int months) {
         return 0;
     }
 
-    public int totalAmount(int sum, int percent, int months) {
+    public double totalAmount(int sum, int percent, int months) {
         return 0;
     }
 
-    public int allOverpayments(int sum, int percent, int months) {
+    public double allOverpayments(int sum, int percent, int months) {
         return 0;
     }
 }
