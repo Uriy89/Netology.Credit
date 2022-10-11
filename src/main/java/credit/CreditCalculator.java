11794b6e72eadc10f730b9210f2f4d2070232f0b
Averianov Yuriy <averianovj@gmail.com>
2022-10-11 20:29:07 +0300

    first commit

---
+++ src/main/java/credit/CreditCalculator.java
@@ -0,0 +1,16 @@
+package credit;
+
+public class CreditCalculator {
+
+    public int monthPaymentCalc(int sum, int percent, int mounts) {
+        return 0;
+    }
+
+    public int totalAmount(int sum, int percent, int mounts) {
+        return 0;
+    }
+
+    public int allOverpayments(int sum) {
+        return 0;
+    }
+}
---
+++ src/test/java/credit/CreditCalculatorTest.java
@@ -0,0 +1,48 @@
+package credit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
+
+import java.text.DecimalFormat;
+
+
+class CreditCalculatorTest {
+
+    @Test
+    void monthPaymentCalc() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        CreditCalculator calculator = new CreditCalculator();
+        int sum = 100_000;
+        int percent = 13;
+        int months = 24;
+        double monthPercent = Double.parseDouble(decimalFormat.format(months / percent));
+        double monthPay = Double.parseDouble(decimalFormat.format(sum * monthPercent / 100));
+        double res = Double.parseDouble(decimalFormat.format(monthPay + (sum / months)));
+        assertTrue(res == calculator.monthPaymentCalc(sum, percent, months));
+    }
+
+    @Test
+    void totalAmount() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        CreditCalculator calculator = new CreditCalculator();
+        int sum = 100_000;
+        int percent = 13;
+        int months = 24;
+        double monthPercent = Double.parseDouble(decimalFormat.format(months / percent));
+        double res = Double.parseDouble(decimalFormat.format(monthPercent + sum));
+        assertTrue(res == calculator.monthPaymentCalc(sum, percent, months));
+    }
+
+    @Test
+    void allOverpayments() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        CreditCalculator calculator = new CreditCalculator();
+        int sum = 100_000;
+        int percent = 13;
+        int months = 24;
+        double monthPercent = Double.parseDouble(decimalFormat.format(months / percent));
+        double allPay = Double.parseDouble(decimalFormat.format(monthPercent + sum));
+        double res = Double.parseDouble(decimalFormat.format(allPay - sum));
+        assertTrue(res == calculator.monthPaymentCalc(sum, percent, months));
+    }
+}
