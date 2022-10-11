f0dbe3781993d1fe8053c852be6e077e1f8e5cf1
Averianov Yuriy <averianovj@gmail.com>
2022-10-11 20:36:48 +0300

    final commit

--- src/main/java/credit/CreditCalculator.java
+++ src/main/java/credit/CreditCalculator.java
@@ -1,18 +1,28 @@
 package credit;
 
 
+import java.text.DecimalFormat;
+
 public class CreditCalculator {
 
-
+    private final DecimalFormat decimalFormat = new DecimalFormat("#.##");
     public double monthPaymentCalc(int sum, int percent, int months) {
-        return 0;
+        double monthPercent = Double.parseDouble(decimalFormat.format(months / percent));
+        double monthPay = Double.parseDouble(decimalFormat.format(sum * monthPercent / 100));
+        double res = Double.parseDouble(decimalFormat.format(monthPay + (sum / months)));
+        return res;
     }
 
     public double totalAmount(int sum, int percent, int months) {
-        return 0;
+        double monthPercent = Double.parseDouble(decimalFormat.format(months / percent));
+        double res = Double.parseDouble(decimalFormat.format(monthPercent + sum));
+        return res;
     }
 
     public double allOverpayments(int sum, int percent, int months) {
-        return 0;
+        double monthPercent = Double.parseDouble(decimalFormat.format(months / percent));
+        double allPay = Double.parseDouble(decimalFormat.format(monthPercent + sum));
+        double res = Double.parseDouble(decimalFormat.format(allPay - sum));
+        return res;
     }
 }
